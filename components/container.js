6ebc70ca89e00f5feb39ffaf006aab79a8f47a01
dovelyyyy🍓 <weihanxin@dovelyyyyyydeMacBook-Pro.local>
2022-05-17 01:29:46 -0400

    fixed register template

--- components/container.js
+++ components/container.js
@@ -1,6 +1,7 @@
 import styles from './container.module.scss'
-export default function Container({children}) {
-    return <div className={styles.container}>
-        {children}
-        </div>
+
+export default function Container({ children }) {
+ return <div className={styles.container}>
+  {children}
+ </div>
 }
